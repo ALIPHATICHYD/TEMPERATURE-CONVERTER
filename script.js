a45406d680f5fe0577c95d46fb6b57e560271f49
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-08-30 17:55:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ window.onload = function() {
     c.oninput = function() {
         f.value = (c.value * 9/5) + 32;
     };
+    
+        f.oninput = function() {
+        c.value = (f.value - 32) * 5/9;
+    }
+  };
